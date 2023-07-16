d43cc0d1f1dab097272391008d18ad844d1d1ad4
chaunnt <chaupad@gmail.com>
2023-07-16 09:53:00 +0700

    xoa bot code

--- flutter/lib/mobile/pages/home_page.dart
+++ flutter/lib/mobile/pages/home_page.dart
@@ -128,18 +128,18 @@ class _HomePageState extends State<HomePage> {
     // if (isAndroid) {
       // _pages.addAll([WebViewChatPage(), ServerPage()]);
       _pages.add(ServerPage(
-        // callback: callBackAuthSuccess,
+        callback: callBackAuthSuccess,
       ));
       // _pages.add(MyPasscodePage());
     // }
     // _pages.add(SettingsPage());
   }
 
-  // void callBackAuthSuccess () {
-  //   setState(() {
-  //     _selectedIndex = 0;
-  //   });
-  // }
+  void callBackAuthSuccess () {
+    setState(() {
+      _selectedIndex = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
