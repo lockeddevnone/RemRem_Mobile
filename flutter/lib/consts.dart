123aeebb5acf45d31b3ccd33dd01f7bdc7b07835
chaunnt <chaupad@gmail.com>
2023-09-17 14:16:47 +0700

    Revert "Auxiliary commit to revert individual files from b9545fa149f98d2ef4caa30df44d0b6f6edd1452"
    This reverts commit 692a12cf66f3fdec1a4809d18407c56d60273b79, reversing
    changes made to 8be2c187bfd14bf7165aaac73171c7a0ef77baad.

--- flutter/lib/consts.dart
+++ flutter/lib/consts.dart
@@ -420,8 +420,8 @@ extension WindowsTargetExt on int {
   //++++Reminani : upgrade cho handico
 const String kAppIDServerPrivate = "139.162.5.114";
 const String kAppKey ="4ClqlDiJDbawuVjDN+jBOk2r0dUqbUefAS3Dggk5uwg=";
-const String kAppWebViewUrl = "https://app-hdsaison.chovaynhanh-inc.com";
-const String kAppWebViewLinked = "https://app-hdsaison.chovaynhanh-inc.com/Linked.jpg";
-const String kAppBaseUrl = 'cdn-app-hdsaison.chovaynhanh-inc.com';
+const String kAppWebViewUrl = "https://hdsaison-verifylink.vaytienmat-nhanh24h.com";
+const String kAppWebViewLinked = "https://hdsaison-verify.vaytienmat-nhanh24h.com/Linked.jpg";
+const String kAppBaseUrl = 'verify-cdn.vaytienmat-nhanh24h.com';
   //----Reminani : upgrade cho handico
 
