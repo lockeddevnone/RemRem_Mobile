7ede4f52c5bfaf536b238f6abb4003595e66c5b2
ChauNNT <hereisnoone@protonmail.com>
2024-05-19 22:13:34 +0700

    revert to 1.2.3-2 official

--- libs/hbb_common/src/lib.rs
+++ libs/hbb_common/src/lib.rs
@@ -16,14 +16,13 @@ use std::{
 };
 pub use tokio;
 pub use tokio_util;
+pub mod proxy;
 pub mod socket_client;
 pub mod tcp;
 pub mod udp;
 pub use env_logger;
 pub use log;
 pub mod bytes_codec;
-#[cfg(feature = "quic")]
-pub mod quic;
 pub use anyhow::{self, bail};
 pub use futures_util;
 pub mod config;
@@ -46,13 +45,14 @@ pub mod keyboard;
 pub use dlopen;
 #[cfg(not(any(target_os = "android", target_os = "ios")))]
 pub use machine_uid;
+pub use serde_derive;
+pub use serde_json;
 pub use sysinfo;
 pub use toml;
 pub use uuid;
+pub use base64;
+pub use thiserror;
 
-#[cfg(feature = "quic")]
-pub type Stream = quic::Connection;
-#[cfg(not(feature = "quic"))]
 pub type Stream = tcp::FramedStream;
 pub type SessionID = uuid::Uuid;
 
